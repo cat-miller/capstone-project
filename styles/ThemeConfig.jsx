811c1f26320383456401076acd2099e0967c6b8e
Carolin Moellercat-miller <github@carolinmoeller.com>
2022-06-01 16:48:04 +0200

    refine default theme

--- styles/ThemeConfig.jsx
+++ styles/ThemeConfig.jsx
@@ -1,11 +1,11 @@
 export const lightTheme = {
-  headline: 'hsl(248, 89%, 60%)',
-  headlinebg: 'hsl(266, 100%, 98%)',
+  headline: 'hsl(103, 100%, 40%)',
+  headlinebg: 'hsl(197, 100%, 97%)',
   navigationtext: 'hsl(0, 0%, 100%)',
-  navigationbg: 'hsl(248, 89%, 69%)',
-  navigationactive: 'hsl(248, 89%, 60%)',
-  bodybg: 'hsl(266, 100%, 98%)',
-  overlaybg: 'hsla(0, 0%, 100%, 0.7)',
+  navigationbg: 'hsl(334, 100%, 70%)',
+  navigationactive: 'hsl(334, 100%, 62%)',
+  bodybg: 'hsl(197, 100%, 97%)',
+  overlaybg: 'hsla(197, 100%, 97%, 0.7)',
   themebutton: 'hsl(313, 100%, 65%)',
   themebuttonbg: 'hsl(313, 100%, 95%)',
 };
@@ -19,5 +19,17 @@ export const darkTheme = {
   bodybg: 'hsl(248, 65%, 12%)',
   overlaybg: 'hsla(248, 65%, 12%, 0.7)',
   themebutton: 'hsl(9, 100%, 65%)',
-  themebuttonbg: 'hsl(9, 100%, 95%)',
+  themebuttonbg: 'hsl(9, 100%, 85%)',
+};
+
+export const cherryTheme = {
+  headline: 'hsl(334, 100%, 60%)',
+  headlinebg: 'hsl(266, 100%, 97%)',
+  navigationtext: 'hsl(0, 0%, 100%)',
+  navigationbg: 'hsl(334, 100%, 69%)',
+  navigationactive: 'hsl(334, 100%, 60%)',
+  bodybg: 'hsl(266, 100%, 97%)',
+  overlaybg: 'hsla(0, 0%, 100%, 0.7)',
+  themebutton: 'hsl(313, 100%, 65%)',
+  themebuttonbg: 'hsl(313, 100%, 95%)',
 };
