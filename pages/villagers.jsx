3ca85a4e0e964f1fcc35d2f68a8ed02255f12e30
Carolin Moellercat-miller <github@carolinmoeller.com>
2022-06-13 11:53:59 +0200

    Toggle NeighborCards onClick

--- pages/villagers.jsx
+++ pages/villagers.jsx
@@ -2,9 +2,10 @@ import StyledCardsWrapper from '../components-styled/StyledCardsWrapper';
 import VillagerCard from '../components/VillagerCard';
 import PageWrapper from '../components/PageWrapper';
 import StyledHeader from '../components-styled/StyledHeader';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { selectVillagers } from '../features/villagers/villagerSlice';
 import StyledButton from '../components-styled/StyledButton';
+import { useState } from 'react';
 
 export default function VillagersPage() {
   const {
@@ -12,19 +13,43 @@ export default function VillagersPage() {
     neighbors,
     data: villagers,
   } = useSelector(selectVillagers);
+  const [showNeighbors, setShowNeighbors] = useState(false);
+  const [showFavorites, setShowFavorites] = useState(false);
+  const [villagersToShow, setVillagersToShow] = useState(villagers);
+
+  const dispatch = useDispatch();
+
+  function toggleNeighborCards() {
+    setShowNeighbors(!showNeighbors);
+    if (showNeighbors) {
+      setVillagersToShow(
+        neighbors.map(neighborID =>
+          villagers.find(({ id }) => id === neighborID)
+        )
+      );
+    } else {
+      setVillagersToShow(villagers);
+    }
+  }
 
   return (
     <PageWrapper>
       <StyledHeader>
-        <StyledButton className="favorite">
+        <StyledButton className="favorite" isActive={showFavorites}>
           {favorites.length}/{villagers.length}
         </StyledButton>
         Villagers
-        <StyledButton className="neighbor">{neighbors.length}/10</StyledButton>
+        <StyledButton
+          isActive={showNeighbors}
+          className="neighbor"
+          onClick={toggleNeighborCards}
+        >
+          {neighbors.length}/10
+        </StyledButton>
       </StyledHeader>
       <StyledCardsWrapper>
-        {villagers?.map((villager, index) => (
-          <VillagerCard index={index} key={villager.id} villager={villager} />
+        {villagersToShow?.map(villager => (
+          <VillagerCard key={villager.id} villager={villager} />
         ))}
       </StyledCardsWrapper>
     </PageWrapper>
