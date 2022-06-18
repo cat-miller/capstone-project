015505ed478a8cb0c5e341c7b1942fffff5b11ad
Carolin Moellercat-miller <github@carolinmoeller.com>
2022-06-18 12:04:15 +0200

    Add Form and Formik

--- components/PassportForm.jsx
+++ components/PassportForm.jsx
@@ -1,12 +1,24 @@
+import { Formik, Form, Field } from 'formik';
+
 export default function PassportForm() {
   return (
-    <form>
-      <label>
-        <input placeholder="NAME " />
-      </label>
-      <label>
-        <input placeholder="NAME ISLAND" />
-      </label>
-    </form>
+    <Formik
+      initialValues={{
+        island: { user: '', name: '', fruit: '', hemisphere: '' },
+        code: { switch: '', dreamId: '', creatorId: '' },
+      }}
+      onSubmit={values => console.log(values)}
+    >
+      <Form>
+        <Field name="island.user" placeholder="USER NAME" />
+        <Field name="island.name" placeholder="ISLAND NAME" />
+        <Field name="island.fruit" placeholder="FRUIT" />
+        <Field name="island.hemisphere" placeholder="HEMISPHERE" />
+        <Field name="code.switch" placeholder="0000 - 0000 - 0000" />
+        <Field name="code.dreamId" placeholder="0000 - 0000 - 0000" />
+        <Field name="code.creatorId" placeholder="0000 - 0000 - 0000GST" />
+        <button type="submit">Submit</button>
+      </Form>
+    </Formik>
   );
 }
